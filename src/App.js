075c1753b07399748961012bccf8fbf6561d226b
Tanaka Chingonzo <tanaka.chingonzo@atomrain.com>
2021-10-21 11:37:57 -0400

    fullscreen background image

--- src/App.js
+++ src/App.js
@@ -7,18 +7,9 @@ function App() {
     width: '100%'
   };
 
-  const dummyTextdiv = {
-    height: '100%',
-    width: '100%'
-  }
-
-  const dummyText = <div style={dummyTextdiv}>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-  </div>
   return (
-    <div style={container}>{dummyText}{dummyText}{dummyText}{dummyText}{dummyText}{dummyText}{dummyText}{dummyText}</div>
-
+    <div style={container}></div>
   );
 }
-// const background = { backgroundImage: `url(${img})`, height: '100%', width:'100%'}
 
 export default App;
